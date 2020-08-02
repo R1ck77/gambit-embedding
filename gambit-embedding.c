22cecdb612ada0ba488d0966af8ebecea38da267
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2020-08-02 20:40:07 +0200

    Add basic initialization and finalization

--- gambit-embedding.c
+++ gambit-embedding.c
@@ -1,5 +1,17 @@
 #include <gambit.h>
 
+#define SCHEME_LIBRARY_LINKER ___LNK_no_idea__
+
 int main(int argc, char *argv[]) {
+
+  ___setup_params_struct setup_params;
+
+  ___setup_params_reset(&setup_params);
+  ___setup(&setup_params);
+
+  // HIC SUNT LEONES
+  
+  ___cleanup();
+  
   return 0;
 }
