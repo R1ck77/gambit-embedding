306acbe29fef4821f9c6b025292d339c76916aa7
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2020-09-06 20:39:17 +0200

    Add a simple and probably memory-leaking extension

--- test.scm
+++ test.scm
@@ -1,7 +1,10 @@
 
+(c-declare "char *hello_world()
+{
+   return \"Hello, World!\";
+}")
 
-(define (say-hello)
-  "Hello, World!")
+(define say-hello (c-lambda () char-string "hello_world"))
 
 (display (string-append (say-hello) "\n"))
 
