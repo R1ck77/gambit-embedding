a23477ef1f5ede9e4f38c9d083f5480f111853a8
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2020-09-08 18:18:55 +0200

    MWE showing that ther eis no automatic memory collection
    The loop leaks memory, albeit slowly.

--- test.scm
+++ test.scm
@@ -1,11 +1,22 @@
 
-(c-declare "char *hello_world()
+(c-declare "
+#include <string.h>
+
+char *hello_world()
 {
-   return \"Hello, World!\";
+   return strdup(\"Hello, World!\");
 }")
 
 (define say-hello (c-lambda () char-string "hello_world"))
 
 (display (string-append (say-hello) "\n"))
+
+(define (print-forever)
+  (display
+   (string-append
+    (say-hello) "\n"))
+  (print-forever))
+
+(print-forever)
 
 
