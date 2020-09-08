2b3f238c86dd018a60f4b95c9c7194311a75250f
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2020-09-08 19:38:06 +0200

    Minor changes

--- test.scm
+++ test.scm
@@ -1,33 +1,24 @@
 
-;; This one leaks
 (c-declare "
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>")
 
-char *hello_world()
+(c-declare "char *hello_world()
 {
    return strdup(\"Hello, World!\");
 }")
 
-(define say-hello (c-lambda () char-string
-                            " char *output = hello_world();
+(define sane-hello (c-lambda () char-string
+                             "char *output = hello_world();
 #define ___AT_END free(output);
-___return (output); "))
+___return (output);"))
 
-;; This one does not leak
-(c-declare "
-char *echo(char *string)
-{
-    return string;
-} ")
-(define echo (c-lambda (char-string) char-string "echo"))
+(define nice-but-leaking-hello (c-lambda () char-string "hello_world"))
 
-(define (print-forever)
-  (display
-   (string-append
-    (say-hello) "\n"))
-  (print-forever))
+(define (eat-memory)
+  (display (string-append (nice-but-leaking-hello) "\n"))
+  (eat-memory))
 
-(print-forever)
+(eat-memory)
 
 
