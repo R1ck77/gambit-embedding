4cf6de9dfca6ed93009da18dfaaedd169f98a085
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2020-09-20 19:04:49 +0200

    Revert "Doomed attempt at returning multiple values from C"
    This reverts commit a36b7c5ce1022a0f236ced8b564716c3d079b1c3.

--- sdl.scm
+++ sdl.scm
@@ -24,11 +24,11 @@
 (define sdl-log (c-lambda (char-string char-string) void "SDL_Log"))
 (define sdl-get-error (c-lambda () char-string "___return((char *) SDL_GetError());"))
 
-(c-declare "typedef struct _window_renderer_creation_output {
+(c-declare "struct window_renderer_creation_output {
    SDL_Window *window;
    SDL_Renderer *renderer;
    int return_value;
-} window_renderer_creation_output;")
+};")
 
 (c-declare "long int scm_free(void *memory) {
     free(memory);
@@ -36,20 +36,22 @@
 }")
 
 ;; TODO/FIXME static typing
-(define simple-sdl-create-window-and-renderer (c-lambda (scheme-object int int) void
-"SDL_Window *window = malloc(sizeof(*SDL_Window));
-SDL_Renderer *renderer = malloc(sizeof(*SDL_Renderer));
-int return_value = SDL_CreateWindowAndRenderer(___arg1, ___arg2, SDL_WINDOW_RESIZABLE, &window, &renderer);
-___VECTORSET(___arg1, ___FIX(0), window);
-___VECTORSET(___arg1, ___FIX(1), renderer);
-___VECTORSET(___arg1, ___FIX(2), return_value);"))
+(define simple-sdl-create-window-and-renderer (c-lambda (int int) (pointer void (void*) "scm_free")
+"struct window_renderer_creation_output *output = malloc(sizeof(struct window_renderer_creation_output));
+output->return_value = SDL_CreateWindowAndRenderer(___arg1, ___arg2, SDL_WINDOW_RESIZABLE, &output->window, &output->renderer);
+___return(output);"))
+
+;; TODO/FIXME name
+(define get-return-value (c-lambda ((pointer void)) int
+                                   "struct window_renderer_creation_output *output = ___arg1;
+___return(output->return_value);"))
 
 
 (when (not (= 0 (sdl-init sdl-init-everything)))
   (sdl-log  "Some error happened %s!\n" (sdl-get-error))
   (exit 1))
 
-(when (not (= (list-ref (simple-sdl-create-window-and-renderer (values #f #f #f) 320 200) 2) 0))
+(when (not (= (get-return-value (simple-sdl-create-window-and-renderer 320 200)) 0))
   (sdl-log "Unable to create the window and the renderer")
   (exit 2))
 
