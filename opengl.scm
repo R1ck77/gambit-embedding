1695d46cb372b16f3c6250a076968208540d95f2
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2020-09-28 14:09:44 +0200

    Minor changes

--- opengl.scm
+++ opengl.scm
@@ -2,11 +2,11 @@
 
 (c-declare "#include <GL/gl.h>")
 
+;;; Constants
 (define gl-color-buffer-bit (uint-c-constant "GL_COLOR_BUFFER_BIT"))
 
-;; Enough OpenGL calls to show "something"
+;;; Methods
 (define gl-clear-color (c-lambda (float float float float) void "glClearColor"))
 (define gl-clear (c-lambda (int32) void "glClear"))
-;; glClear(GL_COLOR_BUFFER_BIT)
 
 
