3542a33fd7d45d9b865d444760bf44e4bc44fc42
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2020-10-11 14:02:02 +0200

    Poor man's manual test of shaders

--- sdl.scm
+++ sdl.scm
@@ -125,16 +125,29 @@ ___return(SDL_CreateWindowAndRenderer(___arg1, ___arg2, SDL_WINDOW_RESIZABLE, &w
     (apply callback (list event))
     (wait-for-quit event callback)))
 
+
+(define triangle-vertex-shader "uniform mat4 uMVPMatrix;
+attribute vec4 vPosition;
+
+void main() {
+  gl_Position = uMVPMatrix * vPosition;
+}")
+
+(define triangle-fragment-shader "precision mediump float;
+uniform vec4 vColor;
+
+void main() {
+  gl_FragColor = vColor;
+}")
+
 (define (create-shaders)
-  (display (gl-create-shader gl-vertex-shader))
-  (display "\n")
-  (display (gl-create-shader gl-fragment-shader))
+  (display (opengl-program-from-sources triangle-vertex-shader triangle-fragment-shader))
   (display "\n"))
 
 (define window (initialize-opengl-window 640 480))
 (create-shaders)
 (wait-for-quit (create-sdl-event)
-               (lambda (event)
+               (lambda (event)                 
                  (gl-clear-color 0.0 0.0 1.0 1.0)
                  (gl-clear gl-color-buffer-bit)
                  (sdl-gl-swap-window window)
