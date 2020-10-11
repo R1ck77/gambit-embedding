8c939251fdb1474dc2dfe8757033ffe60c317f04
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2020-10-11 21:22:20 +0200

    Add more gambit boilerplate code. Compiles but doesn't work

--- c-demo.c
+++ c-demo.c
@@ -1,5 +1,26 @@
 #include <stdlib.h>
+#define ___VERSION 409003
+#include <gambit.h>
+
+//#define SCHEME_LIBRARY_LINKER ___LNK_demo__
+
+//___BEGIN_C_LINKAGE
+//extern ___mod_or_lnk SCHEME_LIBRARY_LINKER (___global_state);
+//___END_C_LINKAGE
+
 
 int main(void) {
+  ___setup_params_struct setup_params;
+  ___setup_params_reset (&setup_params);
+
+  setup_params.version = ___VERSION;
+  //  setup_params.linker  = SCHEME_LIBRARY_LINKER;
+
+  ___setup (&setup_params);
+
+  // Meat
+
+  ___cleanup ();
+
   return 0;
 }
